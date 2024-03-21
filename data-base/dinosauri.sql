2a04f0768a2708ac686672047942d17907018f09
Alessandra <a.zaino2@studenti.unisa.it>
2024-03-21 11:03:44 +0100

    Add files via upload

--- data-base/dinosauri.sql
+++ data-base/dinosauri.sql
@@ -8,7 +8,7 @@ id varchar(255) primary key,
 nome varchar (100) not null,
 categoria varchar (50) not null,
 lunghezza float not null,
-dieta varchar (50) not null,
+alimentazione varchar (50) not null,
 regione_geografica varchar (100) not null,
 prezzo float not null,
 descrizione varchar (255) not null
@@ -27,12 +27,18 @@ password_utente varchar(255) not null,
 nome varchar(255) not null,
 cognome varchar(255) not null, 
 email varchar(255) not null,
+primary key (email)
+);
+
+create table spedizione (
+nome varchar (255) not null,
+cognome varchar (255) not null,
 via varchar(255) not null,
 cap char(5) not null,
 provincia varchar(255) not null,
 comune varchar(255) not null,
 numero_civico varchar(255) not null,
-primary key (email)
+primary key(nome, cognome)
 );
 
 create table admin (
