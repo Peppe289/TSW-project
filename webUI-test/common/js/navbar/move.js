9d68582b6d472a1b058a30afae6325327022ebe7
Peppe289 <gsperanza204@protonmail.com>
2024-05-05 11:04:57 +0200

    sistema l'ombra nella navbar
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- webUI-test/common/js/navbar/move.js
+++ webUI-test/common/js/navbar/move.js
@@ -5,12 +5,8 @@ function openNav() {
 
     if (!isOpen) {
         navBarStyle.left = "0";
-        // aggiungi l'ombra che è figa
-        navBarStyle.boxShadow = "0 0 15px rgba(0, 0, 0, 0.5)";
     } else {
-        navBarStyle.left = "-250px";
-        // Rimuovi l'ombra, altrimenti si vede di lato 
-        navBarStyle.boxShadow = "";
+        navBarStyle.left = "-260px";
     }
 
     isOpen = !isOpen;
