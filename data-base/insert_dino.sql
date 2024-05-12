efeee1a18d81956160c4029d1d1dfa77f998a13d
Peppe289 <gsperanza204@protonmail.com>
2024-05-12 13:24:33 +0200

    database: aggiorna gli insert
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- data-base/insert_dino.sql
+++ data-base/insert_dino.sql
@@ -1,22 +1,40 @@
 USE ecommerce;
 
-INSERT INTO prodotto (id_categoria, nome, descrizione, prezzo, disponibilita, quantita, alimentazione, categoria) VALUES
-('TR','T-rex', 'predatore gigantesco e uno dei più famosi dinosauri carnivori. Si ritiene che cacciasse grandi erbivori', 21.00,'SI', 30, 'carnivoro', 'rettile'),
-('BR','Brachiosauro', 'Il Brachiosaurus era un grande dinosauro erbivoro caratterizzato dal lungo collo e le zampe anteriori più lunghe delle zampe posteriori.',25.50,  'SI', 25,'erbivoro', 'rettile'),
-('ST', 'Stegosauro', 'Aveva piccoli denti piatti adatti a triturare il materiale vegetale.', 18.00,  'SI', 38, 'erbivoro','rettile'),
-('VL', 'Velociraptor', 'Si ritiene che cacciasse in branco e si nutrisse di piccoli animali come piccoli dinosauri, rettili e insetti.', 15.00, 'SI', 50, 'carnivoro','rettile'),
-('TC', 'Triceratopo', 'Aveva un becco robusto e denti adatti per strappare e triturare il cibo.', 17.00, 'SI', 60,'erbivoro', 'rettile'),
-('PT', 'Pterodattilo', 'Era uno dei più grandi e più conosciuti dinosauri. Aveva una apertura alare di oltre 6 metri e si ritiene che fosse principalmente un pescatore.', 17.5, 'SI', 26, 'carnivoro', 'uccello'),
-('UTR', 'Uovo di T-rex', 'Uovo di T-rex trovato in buono stato', 10.5 ,'SI', 30, NULL, NULL),
-('UBR', 'Uovo di Brachiosauro', 'Uovo di Brachiosauro parzialmente danneggiato', 15.0 , 'SI', 52, NULL, NULL),
-('UST', 'Uovo di Stegosauro', 'Uovo di Stegosauro ben conservato', 7.99, 'SI', 20, NULL, NULL),
-('UVL', 'Uovo di Velociraptor', 'Uovo di Velociraptor ben conservato',10.00 , 'SI', 25, NULL, NULL),
-('UTC', 'Uovo di Triceratopo', 'Uovo di Triceratopo leggermente aperto', 6.99, 'SI', 10, NULL, NULL),
-('UPT', 'Uovo di Pterodattilo', 'Uovo di Pterodattilo ben conservato', 8.50, 'SI', 34, NULL, NULL), 
-('OC', 'Osso per dinosauro Carnivoro', 'Osso grande per dinosauro carnivoro', 25.00, 'SI', 20, NULL, NULL),
-('OE', 'Osso per dinosauro Erbivoro', 'Osso piccolo per dinosauro erbivoro', 15.00, 'SI', 30, NULL, NULL),
-('GM', 'Guizaglio medio', 'Guinzaglio in pelle per dinosauro di taglia media', 20.00, 'SI', 15, NULL, NULL),
-('GG', 'Guinzaglio gigante', 'Guinzaglio robusto per dinosauro gigante', 30.00, 'SI', 10, NULL, NULL);
+INSERT INTO prodotto (id_prodotto, nome, photo_path, descrizione, alimentazione, categoria, prezzo) VALUES
+('TR', 'T-rex', NULL, 'predatore gigantesco e uno dei più famosi dinosauri carnivori. Si ritiene che cacciasse grandi erbivori', 'carnivoro', 'rettile',  21.00),
+('BR', 'Brachiosauro', NULL, 'Il Brachiosaurus era un grande dinosauro erbivoro caratterizzato dal lungo collo e le zampe anteriori più lunghe delle zampe posteriori.', 'erbivoro', 'rettile',  25.50),
+('ST', 'Stegosauro', NULL, 'Aveva piccoli denti piatti adatti a triturare il materiale vegetale.', 'erbivoro','rettile',  18.00),
+('VL', 'Velociraptor', NULL, 'Si ritiene che cacciasse in branco e si nutrisse di piccoli animali come piccoli dinosauri, rettili e insetti.', 'carnivoro', 'rettile',  15.00),
+('TC', 'Triceratopo', NULL, 'Aveva un becco robusto e denti adatti per strappare e triturare il cibo.', 'erbivoro', 'rettile',  17.00),
+('PT', 'Pterodattilo', NULL, 'Era uno dei più grandi e più conosciuti dinosauri. Aveva una apertura alare di oltre 6 metri e si ritiene che fosse principalmente un pescatore.', 'carnivoro', 'uccello',  17.50),
+('UTR', 'Uovo di T-rex', NULL, 'Uovo di T-rex trovato in buono stato', NULL, 'uovo',  10.5),
+('UBR', 'Uovo di Brachiosauro', NULL, 'Uovo di Brachiosauro parzialmente danneggiato', NULL, 'uovo',  15.0),
+('UST', 'Uovo di Stegosauro', NULL, 'Uovo di Stegosauro ben conservato', NULL, 'uovo',  7.99),
+('UVL', 'Uovo di Velociraptor', NULL, 'Uovo di Velociraptor ben conservato', NULL, 'uovo',  10.0),
+('UTC', 'Uovo di Triceratopo', NULL, 'Uovo di Triceratopo leggermente aperto', NULL, 'uovo',  6.99),
+('UPT', 'Uovo di Pterodattilo', NULL, 'Uovo di Pterodattilo ben conservato', NULL, 'uovo',  8.50),
+('OC', 'Osso per dinosauro Carnivoro', NULL, 'Osso grande per dinosauro carnivoro', NULL, 'osso',  25.00),
+('OE', 'Osso per dinosauro Erbivoro', NULL, 'Osso piccolo per dinosauro erbivoro', NULL, 'osso',  15.00),
+('GM', 'Guizaglio medio', NULL, 'Guinzaglio in pelle per dinosauro di taglia media', NULL, 'guinzaglio',  20.00),
+('GG', 'Guinzaglio gigante', NULL, 'Guinzaglio robusto per dinosauro gigante', NULL, 'guinzaglio',  30.00);
+
+INSERT INTO elemento_prodotto (id_prodotto, prezzo, disponibilita) VALUES
+('TR', NULL, true),
+('BR', NULL, true),
+('ST', NULL, true),
+('VL', NULL, true),
+('TC', NULL, true),
+('PT', NULL, true),
+('UTR', NULL, true),
+('UBR', NULL, true),
+('UST', NULL, true),
+('UVL', NULL, true),
+('UTC', NULL, true),
+('UPT', NULL, true),
+('OC', NULL, true),
+('OE', NULL, true),
+('GM', NULL, true),
+('GG', NULL, true);
 
 INSERT INTO utente (password_utente, nome, cognome, email) VALUES 
 (sha1('ciao1'), 'Mario', 'Rossi', 'mario.rossi@gmail.com'),
