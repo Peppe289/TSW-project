dfb3d11bcd427aad484ea9fc89b9d1a06aaa95cd
Peppe289 <gsperanza204@protonmail.com>
2024-05-28 00:35:16 +0200

    dashboard.js: aggiusta l'update dinamico delle statistiche server

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -1,11 +1,10 @@
 const container = document.getElementById("list");
-var serverStat = false;
 var updateStats;
-var updateStats_btn = document.getElementById("checkbox_area").checked;
-document.getElementById("checkbox_area").addEventListener("change", (event) => {
-    if (event.target.checked && serverStat) {
+let first = 1;
+document.getElementById("checkbox_area").getElementsByTagName("input")[0].addEventListener("change", (event) => {
+    if (event.target.checked) {
         updateStats = setInterval(sererStatsPage, 1000);
-    } else if (!event.target.checked && serverStat) {
+    } else if (!event.target.checked) {
         clearInterval(updateStats);
     }
 });
@@ -22,7 +21,6 @@ document.getElementById("webStat").addEventListener("click", (event) => {
 
 document.getElementById("serverStat").addEventListener("click", (event) => {
     cleanupSelect(event.currentTarget);
-    serverStat = !serverStat;
     document.getElementById("checkbox_area").classList.remove("hide");
     sererStatsPage();
 });
@@ -32,25 +30,37 @@ function cleanupSelect(val) {
     for (var i = 0; i < arr.length; ++i) {
         arr[i].classList.remove("active");
     }
-
+    first = 1;
     val.classList.add("active");
-    updateStats_btn = false;
+    document.getElementById("checkbox_area").getElementsByTagName("input")[0].checked = false;
     clearInterval(updateStats);
     document.getElementById("checkbox_area").classList.add("hide");
 }
 
 function sererStatsPage() {
-    container.innerHTML = '';
+    if (first) container.innerHTML = '';
 
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         var json = JSON.parse(this.responseText);
-        container.innerHTML += '<div>Max time: ' + json.maxTime + '</div>';
-        container.innerHTML += '<div>Bytes Ricevuti: ' + json.bytesReceived + '</div>';
-        container.innerHTML += '<div>Bytes Inviati: ' + json.bytesSent + '</div>';
-        container.innerHTML += '<div>Richieste: ' + json.requestCount + '</div>';
-        container.innerHTML += '<div>Numero di errori: ' + json.errorCount + '</div>';
-        container.innerHTML += '<div>Tempo di servizio: ' + json.processingTime + '</div>';
+
+        if (first) {
+            container.innerHTML += '<div>Max time: ' + json.maxTime + '</div>';
+            container.innerHTML += '<div>Bytes Ricevuti: ' + json.bytesReceived + '</div>';
+            container.innerHTML += '<div>Bytes Inviati: ' + json.bytesSent + '</div>';
+            container.innerHTML += '<div>Richieste: ' + json.requestCount + '</div>';
+            container.innerHTML += '<div>Numero di errori: ' + json.errorCount + '</div>';
+            container.innerHTML += '<div>Tempo di servizio: ' + json.processingTime + '</div>';
+            first = 0;
+        } else {
+            const children = container.getElementsByTagName("div");
+            children[0].innerHTML = "Max Time: " + json.maxTime;
+            children[1].innerHTML = "Bytes Ricevuti: " + json.bytesReceived;
+            children[2].innerHTML = "Bytes Inviati: " + json.bytesSent;
+            children[3].innerHTML = "Richieste: " + json.requestCount;
+            children[4].innerHTML = "Numero di errori: " + json.errorCount;
+            children[5].innerHTML = "Tempo di servizio: " + json.processingTime;
+        }
     }
     xhttp.open("GET", "stats?reason=serverstats", true);
     xhttp.send();
@@ -61,18 +71,10 @@ function webStatPage() {
 }
 
 function generalPage() {
-    const btn = new Map([
-        ['Gestione prodotti',
-            'Aggiungi, rimuovi, modifica prezzo, aggiungi sconti.'],
-        ['Gestione Utenti',
-            'Rimuovi gli utenti, resetta password, gestisci gli admin.']
-    ]);
+    const btn = new Map([['Gestione prodotti', 'Aggiungi, rimuovi, modifica prezzo, aggiungi sconti.'], ['Gestione Utenti', 'Rimuovi gli utenti, resetta password, gestisci gli admin.']]);
 
     container.innerHTML = '';
     btn.forEach((val, key) => {
-        container.innerHTML += '<div><p>' +
-            key + '</p><br><p>' +
-            val + '</p>' +
-            '</div>';
+        container.innerHTML += '<div><p>' + key + '</p><br><p>' + val + '</p>' + '</div>';
     });
 }
