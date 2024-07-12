18ced3845deb1140726633e8eb4176c11a9a7e7f
Peppe289 <gsperanza204@protonmail.com>
2024-07-12 14:00:45 +0200

    Admin: add function for delete/add user
    only admin 1 can be delete or add user.

--- src/main/java/org/dinosauri/dinosauri/model/AdminDAO.java
+++ src/main/java/org/dinosauri/dinosauri/model/AdminDAO.java
@@ -47,13 +47,57 @@ public class AdminDAO {
         return admins;
     }
 
-    /* TODO: implement insert */
-    public static Admin insertInDatabase(String password) throws SQLException {
+    /**
+     * Delete admin from id
+     *
+     * @param id - admin ID
+     */
+    public static boolean deleteAdmin(int id) {
+        if (id <= 1)
+            return false;
+
+        try (Connection con = ConnectionService.getConnection()) {
+            PreparedStatement ps = con.prepareStatement("DELETE FROM amministratore WHERE identificativo = ?");
+            ps.setInt(1, id);
+            ps.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Helper function used in this class for get id of new admin using password.
+     *
+     * @param password - password SHA1 to find.
+     * @return - id of admin with this password
+     * @throws SQLException - sql error.
+     */
+    private static int doRetrieveIDfromPassword(String password) throws SQLException {
+        int id = 0;
         Connection con = ConnectionService.getConnection();
-        PreparedStatement ps = con.prepareStatement("insert into amministratore(password) values (SHA1(?))");
+        PreparedStatement ps = con.prepareStatement("SELECT identificativo FROM amministratore WHERE password = SHA1(?)");
+        ps.setString(1, password);
+        ResultSet rs =ps.executeQuery();
+        if (rs.next())
+            id = rs.getInt("identificativo");
+
+        return id;
+    }
+
+    /**
+     * Create new admin using only password.
+     * Then retrieve id from password to see id for this new admin.
+     *
+     * @param password - string random password
+     * @return - admin id
+     * @throws SQLException - sql error.
+     */
+    public static int insertInDatabase(String password) throws SQLException {
+        Connection con = ConnectionService.getConnection();
+        PreparedStatement ps = con.prepareStatement("INSERT INTO amministratore(password) VALUES (SHA1(?))");
         ps.setString(1, password);
         ps.execute();
-
-        return new Admin();
+        return doRetrieveIDfromPassword(password);
     }
 }
