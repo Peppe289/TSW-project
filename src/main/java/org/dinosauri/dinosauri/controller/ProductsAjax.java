781a4f2dbfd85d2ac80ed8b4cf9716135d967269
Peppe289 <gsperanza204@protonmail.com>
2024-07-16 11:13:01 +0200

    ProductsAjax: add code for get json about category or nutrition.

--- src/main/java/org/dinosauri/dinosauri/controller/ProductsAjax.java
+++ src/main/java/org/dinosauri/dinosauri/controller/ProductsAjax.java
@@ -12,9 +12,25 @@ import java.util.*;
 @WebServlet("/products-json")
 public class ProductsAjax extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List<Product> products = ProductDAO.doRetrieveProducts();
+        String json =  null;
+        String reason = request.getParameter("reason");
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(products);
+
+        switch (reason) {
+            case "cat" -> {
+                List<String> cat = ProductDAO.doRetrieveCategories();
+                json = mapper.writeValueAsString(cat);
+            }
+            case "nut" -> {
+                List<String> nut = ProductDAO.doRetrieveNutrition();
+                json = mapper.writeValueAsString(nut);
+            }
+            case null, default -> {
+                List<Product> products = ProductDAO.doRetrieveProducts();
+                json = mapper.writeValueAsString(products);
+            }
+        }
+
         response.setContentType("application/json");
         response.getWriter().print(json);
     }
